5f1d0648f08dec71bd1543a454d9e62f4f43472d
Svahnen <svahn.jonny@gmail.com>
2022-05-02 15:11:30 +0200

    List and remove books

--- src/main/java/com/example/springdemo/controllers/BookController.java
+++ src/main/java/com/example/springdemo/controllers/BookController.java
@@ -2,16 +2,32 @@ package com.example.springdemo.controllers;
 
 import com.example.springdemo.models.Book;
 import com.example.springdemo.models.BookLombok;
+import com.example.springdemo.models.BooksDao;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class BookController {
 
-    @RequestMapping("/book")
-    public Book getBook() {
-        return new Book("Svahn Food", "Svahnen", 12345);
+    @RequestMapping("/books")
+    public List<Book> getBook() {
+        BooksDao booksDao = BooksDao.getInstance();
+        return booksDao.getBooks();
+    }
+
+    @RequestMapping("/book/{id}")
+    public Book getBook(@PathVariable int id) {
+        BooksDao booksDao = BooksDao.getInstance();
+        return booksDao.getBooks().get(id);
+    }
+
+    @RequestMapping("/book/{idNr}/delete")
+    public void deleteBook(@PathVariable int idNr) {
+        BooksDao booksDao = BooksDao.getInstance();
+        booksDao.deleteBook(idNr);
     }
 
     @RequestMapping("/booklombok")
---
+++ src/main/java/com/example/springdemo/models/BooksDao.java
@@ -0,0 +1,43 @@
+package com.example.springdemo.models;
+
+import java.util.List;
+
+public class BooksDao {
+    private static BooksDao single_instance = null;
+
+    List<Book> books = new java.util.ArrayList<Book>();
+    int idNr = 0;
+
+    private BooksDao() {
+        books.add(new Book("Svahn Food", "Svahnen", idNr++));
+        books.add(new Book("Some other book", "Svahnen", idNr++));
+        books.add(new Book("Another book entirely", "Svahnen", idNr++));
+        books.add(new Book("Yet another book entirely", "Svahnen", idNr++));
+
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void deleteBook(int idNr) {
+        for (Book book : books) {
+            if (book.getId() == idNr) {
+                books.remove(book);
+                break;
+
+            }
+        }
+    }
+
+    public static BooksDao getInstance() {
+        if (single_instance == null)
+            single_instance = new BooksDao();
+
+        return single_instance;
+    }
+}
